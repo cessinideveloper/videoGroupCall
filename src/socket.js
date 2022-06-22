7f36beb6f291bb44a98c76eb301deaaf0283608e
Divyank <divyankrawat10@gmail.com>
2022-06-22 16:30:26 +0530

    Final Commit

---
+++ src/socket.js
@@ -0,0 +1,5 @@
+import io from "socket.io-client"
+// const sockets = io('http://localhost:3001', { autoConnect: true, forceNew: true });
+// const sockets = io("https://video-chat-hoster1.herokuapp.com")  // hamara server
+const sockets = io("https://myworld-signalling-server.herokuapp.com")  // myworld server
+export default sockets
